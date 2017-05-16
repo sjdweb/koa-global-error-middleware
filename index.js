e5892ae0277298ae92202269882034ff906bcf74
Sean Drumm <sean@seandrumm.co.uk>
2017-05-16 12:37:15 +0100

    v0.0.3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.status = err.statusCode || err.status || 500;
-    ctx.body = err.error || err.message || 'Internal server error';
-    ctx.app.emit('error', err, ctx);
-  }
-};
+module.exports = function () {
+  return async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.statusCode || err.status || 500;
+      ctx.body = err.error || err.message || 'Internal server error';
+      ctx.app.emit('error', err, ctx);
+    }
+  };
+}
